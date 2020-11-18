a98dbfb41f137b9a3cca06f41b0dc52f41deada6
V1 MoDzZz <67358940+V1MoDzZz@users.noreply.github.com>
2020-11-18 20:59:25 +0500

    Create Tracer4linux.py
    Requirements:
    
    1. Python3
    
    2. phonenumbers ~ commands: pip install phonenumbers

---
+++ Tracer4linux.py
@@ -0,0 +1,57 @@
+import phonenumbers
+
+import sys
+
+import os
+
+os.system('clear')
+
+sys.ps1 = '\033[01;32m '
+print(sys.ps1)
+
+print('''
+
+ ####### ######   #####   #####  
+    #    #     # #     # #     # 
+    #    #     #       #       # 
+    #    ######   #####   #####  
+    #    #   #         #       # 
+    #    #    #  #     # #     # 
+    #    #     #  #####   #####  
+                                 
+
+''')
+
+from phonenumbers import geocoder
+
+number = input("Enter Phone Number to Trace: ")
+
+ch_nmb = phonenumbers.parse(number, "CH")
+print("Region: ", geocoder.description_for_number(ch_nmb, "en"))
+
+
+from phonenumbers import carrier
+
+service_numb = phonenumbers.parse(number, "RO")
+
+print("Carrier: ", carrier.name_for_number(service_numb, "en"))
+
+
+
+
+
+from phonenumbers import timezone
+
+
+gb_number = phonenumbers.parse(number)
+
+print("TimeZone: ")
+
+lol = list(timezone.time_zones_for_number(gb_number))
+
+print(lol)
+
+
+x = phonenumbers.parse(number)
+
+print(x)
